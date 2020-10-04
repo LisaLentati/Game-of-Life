176e990b927b2ebd8b5264aa2f95c25e88392be8
Lisa <lentatilisa@gmail.com>
2020-10-04 18:02:15 +0200

    fixed function life_step_for_tensors

--- LifeStepFunctions.py
+++ LifeStepFunctions.py
@@ -9,15 +9,18 @@ def life_step_for_arrays(X):
 
 def life_step_for_tensors(X):
     """
+    Not cyclic
     Args:
         X: 3D tensor of shape (batch_size, d1, d2).
     """
+    X_4D = tf.reshape(X, (X.shape[0], X.shape[1], X.shape[2], 1))
+    kernel = tf.Variable(tf.ones(shape=(3,3,1,1)), dtype='float32')
+    kernel[1,1,0,0].assign(0)
+    counts = tf.nn.conv2d(X_4D, filters=kernel, strides=1, padding='VALID')
+    counts = tf.reshape(counts, (counts.shape[0], counts.shape[1], counts.shape[2]))
 
-    X = tf.reshape(X, (X.shape[0], X.shape[1], X.shape[2], 1))
-    kernel = tf.ones(shape=(3,3,1,1))
-    X = tf.nn.conv2d(X, filters=kernel, strides=1, padding='VALID')
-    X = tf.reshape(X, (X.shape[0], X.shape[1], X.shape[2]))
-    return X
+    X_valid = X[:,1:-1,1:-1]
+    return tf.cast((counts == 3) | (tf.cast(X_valid, dtype=bool) & (counts == 2)), dtype='float32')
 
 def life_step_for_tensors_cyclic(X):
     # TODO

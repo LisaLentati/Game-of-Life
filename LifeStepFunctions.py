a5c3be0bf982f9389f2b76ed6303b0da39eb268d
Lisa <lentatilisa@gmail.com>
2020-10-12 11:43:46 +0200

    Added new life step function working with pytorch

--- LifeStepFunctions.py
+++ LifeStepFunctions.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.signal import convolve2d
 import tensorflow as tf
+import torch
+import torch.nn.functional as F
 
 
 def life_step_for_arrays(X):
@@ -22,6 +24,21 @@ def life_step_for_tensors(X):
     X_valid = X[:,1:-1,1:-1]
     return tf.cast((counts == 3) | (tf.cast(X_valid, dtype=bool) & (counts == 2)), dtype='float32')
 
-def life_step_for_tensors_cyclic(X):
-    # TODO
-    return
+def life_step_for_pytorch(X):
+    """
+    Not cyclic
+    Args:
+        X: 3D tensor of shape (batch_size, d1, d2).
+    """
+    X_4D = torch.reshape(X, (X.shape[0], 1, X.shape[1], X.shape[2]))
+    kernel = torch.Tensor([[[[1, 1, 1], [1, 0, 1], [1, 1, 1]]]])
+    
+    counts = F.conv2d(X_4D, weight=kernel)
+    counts = torch.reshape(counts, (counts.shape[0], counts.shape[1], counts.shape[2]))
+
+    X_valid = X[:,1:-1,1:-1].type(torch.uint8)
+
+    x1 = X_valid & (counts == 2)
+
+    x2 = counts == 3
+    return (x1 | x2).type(torch.double)
